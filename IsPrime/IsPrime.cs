d1e1d8ebaafa980c8ff336787e2fcf2277bebfa1
koonik <a.malarska@gmail.com>
2016-05-01 18:30:24 +0200

    primes

---
+++ IsPrime/IsPrime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    class IsPrime
+    {
+        private static int max;
+
+        public IsPrime(int val)
+        {
+           SetMax(val);
+        }
+        
+        public void SetMax(int val)
+        {
+            max = val;
+        }
+
+        private bool[] MakeSieve()
+        {
+            bool[] isPrime = new bool[max + 1];
+            for (int i = 2; i<=max; i++) { isPrime[i] = true; }
+            for (int i = 2; i<=max;i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * 2; j<=max; j+=i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+            return isPrime;
+        }
+
+        public void GetPrimes()
+        {
+            bool[] primes = MakeSieve();
+            for (int i = 2; i<=max;i++)
+            {
+                if (primes[i])
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ IsPrime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPrime isPrime = new IsPrime(Convert.ToInt32(Console.ReadLine()));
+
+            isPrime.GetPrimes();
+            Console.ReadKey();
+        }
+    }
+}
